359ca19ecf37e49b11a56fd8d7dc82d9155b7484
Mark Ivanov <ctark28107537@gmail.com>
2024-12-31 19:05:57 +0300

    Solve LeetCode 2396: Strictly Palindromic Number

---
+++ 2396. Srtictly Palindromic Number/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+class Solution {
+  public:
+    bool isStrictlyPalindromic(int n) {
+      return false;
+    }
+};
+
+int main() {
+  Solution solution;
+
+  std::cout << solution.isStrictlyPalindromic(9) << std::endl;
+  std::cout << solution.isStrictlyPalindromic(4) << std::endl;
+}
